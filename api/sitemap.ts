a9e32acd51e53178c81c75f5daf93ad824158171
Ehijiele1 <ehijiele1@gmail.com>
2026-08-24 18:14:32 +0100

    fix(seo): include all primary routes in sitemap and cache output

--- api/sitemap.ts
+++ api/sitemap.ts
@@ -1,25 +1,39 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 import { getPosts } from '../lib/hashnode';
 
-export default async function handler(req: VercelRequest, res: VercelResponse) {
-    const posts = await getPosts(100);
-    const baseUrl = 'https://vanniejay.com.ng';
+export default async function handler(_req: VercelRequest, res: VercelResponse) {
+  const posts = await getPosts(100);
+  const baseUrl = 'https://vanniejay.com.ng';
+  const staticRoutes = [
+    ['/', 'weekly', '1.0'],
+    ['/about', 'monthly', '0.8'],
+    ['/investment-solutions', 'monthly', '0.9'],
+    ['/products', 'weekly', '0.9'],
+    ['/services', 'monthly', '0.8'],
+    ['/resources', 'weekly', '0.8'],
+    ['/contact', 'monthly', '0.7'],
+    ['/blog', 'daily', '0.9'],
+  ];
 
-    const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-  <url><loc>${baseUrl}/</loc><changefreq>weekly</changefreq><priority>1.0</priority></url>
-  <url><loc>${baseUrl}/about</loc><changefreq>monthly</changefreq><priority>0.8</priority></url>
-  <url><loc>${baseUrl}/blog</loc><changefreq>daily</changefreq><priority>0.9</priority></url>
-  ${posts.map(post => `
+  const urls = staticRoutes.map(([path, changefreq, priority]) =>
+    `  <url><loc>${baseUrl}${path}</loc><changefreq>${changefreq}</changefreq><priority>${priority}</priority></url>`
+  ).join('\n');
+
+  const postUrls = posts.map(post => `
   <url>
     <loc>${baseUrl}/blog/${post.slug}</loc>
     <lastmod>${new Date(post.publishedAt).toISOString().split('T')[0]}</lastmod>
     <changefreq>monthly</changefreq>
     <priority>0.7</priority>
-  </url>`).join('')}
+  </url>`).join('');
+
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${urls}
+${postUrls}
 </urlset>`;
 
-    res.setHeader('Content-Type', 'text/xml');
-    res.write(sitemap);
-    res.end();
+  res.setHeader('Content-Type', 'application/xml; charset=utf-8');
+  res.setHeader('Cache-Control', 'public, s-maxage=3600, stale-while-revalidate=86400');
+  res.status(200).send(sitemap);
 }
